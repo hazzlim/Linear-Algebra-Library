91af01e4fc0db138d8c85f7b62385214266957d5
Hari Limaye <hari.limaye@kcl.ac.uk>
2021-01-26 11:08:27 +0100

    Implement dot product and angle functions

--- Python/main.py
+++ Python/main.py
@@ -1,11 +1,16 @@
-from math import sqrt
+from math import sqrt, acos, degrees
+from decimal import Decimal, getcontext
+
+getcontext().prec = 30
+
 
 class Vector(object):
     def __init__(self, coordinates):
+        self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG = "Cannot normalize the zero vector!"
         try:
             if not coordinates:
                 raise ValueError
-            self.coordinates = tuple(coordinates)
+            self.coordinates = tuple([Decimal(x) for x in coordinates])
             self.dimension = len(coordinates)
 
         except ValueError:
@@ -27,21 +32,51 @@ class Vector(object):
         return Vector([x - y for x, y in zip(self.coordinates, other.coordinates)])
 
     def multiply_scalar(self, scalar):
-        return Vector([scalar * x for x in self.coordinates])
+        return Vector([Decimal(scalar) * x for x in self.coordinates])
 
     def magnitude(self):
         squares_sum = sum([x * x for x in self.coordinates])
-        return sqrt(squares_sum)
+        return Decimal.sqrt(squares_sum)
 
-    def direction(self):
+    def normalized(self):
         try:
-            return self.multiply_scalar((1.0 / self.magnitude()))
+            return self.multiply_scalar((Decimal(1.0) / self.magnitude()))
         except ZeroDivisionError:
-            raise Exception("Cannot normalise the zero vector")
+            raise Exception(self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG)
+
+    def dot_product(self, other):
+        return sum([x * y for x, y in zip(self.coordinates, other.coordinates)])
+
+    def angle(self, other, in_degrees=False):
+        try:
+            n1 = self.normalized()
+            n2 = other.normalized()
+            theta = acos(n1.dot_product(n2))
+            if in_degrees:
+                theta = degrees(theta)
+
+            return theta
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+                raise Exception("Cannot calculate angle with the zero vector")
+            else:
+                raise e
 
 
 if __name__ == '__main__':
-    v1 = Vector([1, 2, 3])
-    v2 = Vector([1, 2, 3])
-    print(v1.add_vector(v2))
+    v1 = Vector([7.887, 4.138])
+    v2 = Vector([-8.802, 6.776])
+    print(v1.dot_product(v2))
+    v3 = Vector([-5.955, -4.904, -1.874])
+    v4 = Vector([-4.496, -8.755, 7.103])
+    print(v3.dot_product(v4))
+
+    v5 = Vector([3.183, -7.627])
+    v6 = Vector([-2.668, 5.319])
+    print(v5.angle(v6))
+
+    v15 = Vector([7.35, 0.221, 5.188])
+    v16 = Vector([2.751, 8.259, 3.985])
+    print(v15.angle(v16))
+
 
--- Scala/linear_algebra_lib.scala
+++ Scala/linear_algebra_lib.scala
@@ -1,4 +1,6 @@
 import scala.math.sqrt
+import scala.math.acos
+import scala.math.toDegrees
 import scala.util.Try
 
 // Types
@@ -16,11 +18,17 @@ case class Vector(coordinates: List[Coordinate]) {
         Vector(coordinates.map(_ * sc))
     def magnitude() : Double =
         sqrt(coordinates.map(x => x * x).sum)
-    def direction() : Option[Vector] = 
-        magnitude match {
-            case 0 => None
-            case m => Some(this * (1.0 / magnitude))
-        }
+    def normalized() : Option[Vector] = 
+        if (magnitude == 0) None else Some(this * (1.0 / magnitude))
+    def dot(v2: Vector) : Double = 
+       (coordinates, v2.coordinates).zipped.map(_ * _).sum
+    def angle(v2: Vector, in_degrees : Boolean = false) : Option[Double] =
+        (normalized, v2.normalized) match {
+            case (None, _) | (_, None) => None
+            case (Some(m1), Some(m2)) =>
+                val rad = acos(m1 dot m2)
+                if (in_degrees) Some(toDegrees(rad)) else Some(rad)    
+        }   
 }
 
 // Scalar Operations (for convenience)
@@ -58,5 +66,20 @@ v9.direction
 val v10 = Vector(List(0, 0))
 v10.magnitude
 v10.direction
+
+val v11 = Vector(List(7.887, 4.138))
+val v12 = Vector(List(-8.802, 6.776))
+v11 dot v12
+val v13 = Vector(List(-5.955, -4.904, -1.874))
+val v14 = Vector(List(-4.496, -8.755, 7.103))
+v13 dot v14
+
+val va = Vector(List(3.183, -7.627))
+val vb = Vector(List(-2.668, 5.319))
+va angle vb
+
+val v15 = Vector(List(7.35, 0.221, 5.188))
+val v16 = Vector(List(2.751, 8.259, 3.985))
+v15.angle(v16, true)
 **/
 
